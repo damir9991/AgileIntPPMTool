d3dc1d2cded74e1bb52e6b71829800757990e6e8
damir9991 <damir9991@gmail.com>
2019-10-27 11:03:26 +0100

    Third commit

---
+++ src/main/java/io/agileintelligence/ppmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.exceptions;
+
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier)
+    {
+        this.projectIdentifier=projectIdentifier;
+    }
+
+}
